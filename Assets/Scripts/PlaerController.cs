7f16890687aca5cf6f6695a76c22384ce274b10e
Ron Welch <mmcelt@yahoo.com>
2020-04-28 13:28:19 -0600

    9 - Creating a Script

---
+++ Assets/Scripts/PlaerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaerController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Rigidbody2D _theRB;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
