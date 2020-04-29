99d99e27d740a8bb33476c821a4d2b6981390d70
Ron Welch <mmcelt@yahoo.com>
2020-04-29 15:41:39 -0600

    24 - Keeping Camera Inside the Map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
 	#region Fields
 
 	[SerializeField] Transform _target;
+	[SerializeField] Tilemap _theMap;
+
+	Vector3 _bottomLeftLimit, _topRightLimit;
 
 	#endregion
 
@@ -15,12 +19,18 @@ public class CameraController : MonoBehaviour
 	void Start() 
 	{
 		_target = PlayerController.Instance.transform;
-		
+		_bottomLeftLimit = _theMap.localBounds.min;
+		_topRightLimit = _theMap.localBounds.max;
 	}
 	
 	void LateUpdate() 
 	{
 		transform.position = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+
+		//keep the camera inside the bounds
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, _bottomLeftLimit.x, _topRightLimit.x), Mathf.Clamp(transform.position.y, _bottomLeftLimit.y, _topRightLimit.y), transform.position.z);
+
+		//
 	}
 	#endregion
 
