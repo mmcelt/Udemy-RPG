81762dac230bc3991e38054635a6afc6f53a9a36
Ron Welch <mmcelt@yahoo.com>
2020-04-29 11:25:56 -0600

    SECTION 5: CAMERA & MAPS - 20 -Controlling the Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Transform _target;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_target = PlayerController.Instance.transform;
+		
+	}
+	
+	void LateUpdate() 
+	{
+		transform.position = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
