684b92cdb98538f2a328159008f49528e0a4801a
Ron Welch <mmcelt@yahoo.com>
2020-04-28 20:00:29 -0600

    19 - Making Sure the Player is Always in Scene

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _player;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		if (!PlayerController.Instance)
+		{
+			Instantiate(_player);
+		}
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
