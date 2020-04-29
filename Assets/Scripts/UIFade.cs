0fbb0b98944014cd47ec29ca5b4fb2e88dc3a90b
Ron Welch <mmcelt@yahoo.com>
2020-04-29 17:07:43 -0600

    28 - Fading In & Out

---
+++ Assets/Scripts/UIFade.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFade : MonoBehaviour
+{
+	#region Fields
+
+	public static UIFade Instance;
+
+	[SerializeField] Image _fadePanel;
+	[SerializeField] float _fadeSpeed;
+
+	bool _shouldFadeToBlack, _shouldFadeFromBlack;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Start() 
+	{
+
+	}
+	
+	void Update() 
+	{
+		if (_shouldFadeToBlack)
+		{
+			_fadePanel.color = new Color(_fadePanel.color.r, _fadePanel.color.g, _fadePanel.color.b, Mathf.MoveTowards(_fadePanel.color.a, 1f, _fadeSpeed * Time.deltaTime));
+
+			if (_fadePanel.color.a == 1f)
+				_shouldFadeToBlack = false;
+		}
+		if (_shouldFadeFromBlack)
+		{
+			_fadePanel.color = new Color(_fadePanel.color.r, _fadePanel.color.g, _fadePanel.color.b, Mathf.MoveTowards(_fadePanel.color.a, 0f, _fadeSpeed * Time.deltaTime));
+
+			if (_fadePanel.color.a == 1f)
+				_shouldFadeFromBlack = false;
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void FadeToBlack()
+	{
+		_shouldFadeToBlack = true;
+		_shouldFadeFromBlack = false;
+	}
+
+	public void FadeFromBlack()
+	{
+		_shouldFadeFromBlack = true;
+		_shouldFadeToBlack = false;
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
