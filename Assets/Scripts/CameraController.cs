fc49fcbe24bd404a0efa312d071b41542d69444d
Ron Welch <mmcelt@yahoo.com>
2020-04-29 15:57:46 -0600

    25 - Hiding the Map Edges

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,6 +11,7 @@ public class CameraController : MonoBehaviour
 	[SerializeField] Tilemap _theMap;
 
 	Vector3 _bottomLeftLimit, _topRightLimit;
+	float _halfHeight, _halfWidth;
 
 	#endregion
 
@@ -19,8 +20,13 @@ public class CameraController : MonoBehaviour
 	void Start() 
 	{
 		_target = PlayerController.Instance.transform;
-		_bottomLeftLimit = _theMap.localBounds.min;
-		_topRightLimit = _theMap.localBounds.max;
+
+		//camera limiting data...
+		_halfHeight = Camera.main.orthographicSize;
+		_halfWidth = _halfHeight * Camera.main.aspect;
+
+		_bottomLeftLimit = _theMap.localBounds.min + new Vector3(_halfWidth, _halfHeight, 0f);
+		_topRightLimit = _theMap.localBounds.max + new Vector3(-_halfWidth, -_halfHeight, 0f);
 	}
 	
 	void LateUpdate() 
@@ -29,8 +35,6 @@ public class CameraController : MonoBehaviour
 
 		//keep the camera inside the bounds
 		transform.position = new Vector3(Mathf.Clamp(transform.position.x, _bottomLeftLimit.x, _topRightLimit.x), Mathf.Clamp(transform.position.y, _bottomLeftLimit.y, _topRightLimit.y), transform.position.z);
-
-		//
 	}
 	#endregion
 
