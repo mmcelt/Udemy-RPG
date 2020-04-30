1badbad84dcded017b1585bcf841c21e14d4bf9a
Ron Welch <mmcelt@yahoo.com>
2020-04-30 09:53:17 -0600

    32 - Creating Dialog Manager & Reading Text

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _dialogBox, _nameBox;
+	[SerializeField] Text _dialogText, _nameText;
+	[SerializeField] string[] _dialogLines;
+	[SerializeField] int _currentLine;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_dialogText.text = _dialogLines[_currentLine];
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
