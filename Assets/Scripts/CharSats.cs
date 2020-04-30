417e5f2ad9fb41e91a59a1a9b81a6349481ee3d2
Ron Welch <mmcelt@yahoo.com>
2020-04-30 14:01:05 -0600

    SECTION 7: LEVELING UP - 38 - Player Stats

---
+++ Assets/Scripts/CharSats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSats : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] string _charName;
+	[SerializeField] Sprite _charImage;
+
+	[SerializeField] int _charLevel = 1;
+	[SerializeField] int _currentEXP;
+
+	[SerializeField] int _currenHP;
+	[SerializeField] int _maxHP = 100;
+	[SerializeField] int _currentMP;
+	[SerializeField] int _maxMP = 30;
+
+	[SerializeField] int _strength;
+	[SerializeField] int _defence;
+	[SerializeField] int _weaponPwr;
+	[SerializeField] int _armorPwr;
+	[SerializeField] string _equippedWpn, _equippedArm;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
