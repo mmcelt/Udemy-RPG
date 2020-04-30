4071b9f1f6589a89a8b4a7e7997e0e75a072350b
Ron Welch <mmcelt@yahoo.com>
2020-04-30 14:33:14 -0600

    39 - Adding a Leveling System

--- Assets/Scripts/CharSats.cs
+++ Assets/Scripts/CharSats.cs
@@ -11,6 +11,10 @@ public class CharSats : MonoBehaviour
 
 	[SerializeField] int _charLevel = 1;
 	[SerializeField] int _currentEXP;
+	[SerializeField] int[] _expToNextLevel;
+	[SerializeField] int _maxLevel = 100;
+	[SerializeField] int _baseEXP = 1000;
+	[SerializeField] float _expToNextLvlMultiplier = 1.03f;
 
 	[SerializeField] int _currenHP;
 	[SerializeField] int _maxHP = 100;
@@ -29,7 +33,10 @@ public class CharSats : MonoBehaviour
 
 	void Start() 
 	{
-		
+		_expToNextLevel = new int[_maxLevel];
+		_expToNextLevel[1] = _baseEXP;
+
+		CalculateExpToNextLevels();
 	}
 	
 	void Update() 
@@ -45,6 +52,12 @@ public class CharSats : MonoBehaviour
 
 	#region Private Methods
 
-
+	void CalculateExpToNextLevels()
+	{
+		for(int i=2; i<_expToNextLevel.Length; i++)
+		{
+			_expToNextLevel[i] = Mathf.FloorToInt(_expToNextLevel[i - 1] * _expToNextLvlMultiplier);
+		}
+	}
 	#endregion
 }
