1eb29d7124874497f774017f23c5733b71b80d12
Ron Welch <mmcelt@yahoo.com>
2020-05-02 11:53:25 -0600

    45 - Opening & Closing the Menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _theMenu;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		if (Input.GetButtonDown("Fire2"))
+		{
+			//toggle the menu panel...
+			bool isOpen = _theMenu.activeSelf;
+			isOpen = !isOpen;
+			_theMenu.SetActive(isOpen);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
