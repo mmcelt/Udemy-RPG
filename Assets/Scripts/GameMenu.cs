2305cffadac58c3a3892cfc81449509ebf4d8494
Ron Welch <mmcelt@yahoo.com>
2020-05-02 17:09:06 -0600

    47 - Setting Up Stat References

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
 	#region Fields
 
 	[SerializeField] GameObject _theMenu;
+	[SerializeField] Text[] _nameTexts, _hpTexts, _mpTexts, _lvlTexts, _expTexts;
+	[SerializeField] Slider[] _expSliders;
+	[SerializeField] Image[] _charImages;
+	[SerializeField] GameObject[] _charStatPanels;
+
+	CharSats[] _playerStats;
 
 	#endregion
 
@@ -26,13 +33,31 @@ public class GameMenu : MonoBehaviour
 			isOpen = !isOpen;
 			_theMenu.SetActive(isOpen);
 			GameManager.Instance._gameMenuOpen = isOpen;
+
+			if (isOpen)
+				UpdateMainStats();
 		}
 	}
 	#endregion
 
 	#region Public Methods
 
+	public void UpdateMainStats()
+	{
+		_playerStats = GameManager.Instance._playerStats;
 
+		for(int i=0; i<_playerStats.Length; i++)
+		{
+			if (_playerStats[i].gameObject.activeSelf)
+			{
+				_charStatPanels[i].SetActive(true);
+			}
+			else
+			{
+				_charStatPanels[i].SetActive(false);
+			}
+		}
+	}
 	#endregion
 
 	#region Private Methods
