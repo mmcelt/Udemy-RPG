6ada16924054008d4e81a46e8316617d76d4bfb5
Ron Welch <mmcelt@yahoo.com>
2020-05-04 10:51:56 -0600

    SECTION 9: INVENTORY: 53 -Making Items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	#region Fields
+
+	[Header("Item Type")]
+	public bool _isItem;
+	public bool _isWeapon;
+	public bool _isArmor;
+	[Header("Item Info")]
+	public Sprite _itemSprite;
+	public string _itemName, _itemDesc;
+	public int _itemValue;
+	[Header("Item Details")]
+	public int _amountToChange;
+	public bool _affectHp, _affectMP, _affectStr, _affectDef;
+	public int _weaponStr, _armorStrength;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
