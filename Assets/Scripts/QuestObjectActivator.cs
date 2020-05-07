22da5fee13232ff391ea0cbd610f7d77785fdcaa
Ron Welch <mmcelt@yahoo.com>
2020-05-07 10:36:38 -0600

    73 - Activating Objects for Quests (updated to 2019.3.13f1)

---
+++ Assets/Scripts/QuestObjectActivator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObjectActivator : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] GameObject _objectToActivate;
+	[SerializeField] string _questToCheck;
+	[SerializeField] bool _activeIfComplete;
+
+	bool _initialCheckCompleted;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		if (!_initialCheckCompleted)
+		{
+			_initialCheckCompleted = true;
+			CheckCompletion();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+	public void CheckCompletion()
+	{
+		if (QuestManager.Instance.CheckIfComplete(_questToCheck))
+		{
+			_objectToActivate.SetActive(_activeIfComplete);
+		}
+	}
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
