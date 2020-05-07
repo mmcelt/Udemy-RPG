4379f1b74bafe3afa245d55f04f1f89ddb3fa606
Ron Welch <mmcelt@yahoo.com>
2020-05-06 18:07:05 -0600

    SECTION 11: QUEST TRACKING: 71 - Making Quests

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+	#region Fields
+
+	public static QuestManager Instance;
+
+	public string[] _questMarkerNames;
+	public bool[] _questMarkersComplete;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else if (Instance != this)
+			Destroy(gameObject);
+	}
+
+	void Start() 
+	{
+		_questMarkersComplete = new bool[_questMarkerNames.Length];
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
