93a9669ecd39362248d9c8830b4552462bd2f08a
Ron Welch <mmcelt@yahoo.com>
2020-05-09 19:14:39 -0600

    88 - Creating Battle Characters

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+	#region Fields
+
+	public bool _isPlayer;
+	public string[] _movesAvailable;
+
+	public string _charName;
+	public int _currentHP, _maxHP, _currentMP, _maxMP, _STR, _DEF, _wpnPwr, _armPwr;
+	public bool _hasDied;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
