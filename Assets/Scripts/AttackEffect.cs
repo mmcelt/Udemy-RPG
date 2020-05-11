c1730b4113860f5e19dfd081aa3eb7efbe6ded79
Ron Welch <mmcelt@yahoo.com>
2020-05-11 11:16:57 -0600

    93 - Creating Attack Effects

---
+++ Assets/Scripts/AttackEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AttackEffect : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _effectLength;
+	[SerializeField] int _SFX;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		AudioManager.Instance.PlaySFX(_SFX);
+		Destroy(gameObject, _effectLength);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
